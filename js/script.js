116f93a62f01d24c85c0d1f8e0dddcd2d81d8b06
github_DASorescu <srsdln97@gmail.com>
2022-06-22 17:24:58 +0200

    declared-constants

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log("CIAO")
+console.log("CIAO")+
+
+const name = document.getElementById("name");
+const surname = document.getElementById("surname");
+const age = document.getElementById("age")
+const travelLenght = document.getElementById("TLenght")
+console.log( name , surname , age , TLenght)
