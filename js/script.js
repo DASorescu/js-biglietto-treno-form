e2bd4ea616e3ccd905bbd674ef26056e6000f4ff
github_DASorescu <srsdln97@gmail.com>
2022-06-22 17:46:36 +0200

    added-some-comments

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 console.log("CIAO")+
+// declaring generic variables
 
+const button = document.getElementById("button");
 const name = document.getElementById("name");
+console.log("name");
 const surname = document.getElementById("surname");
-const age = document.getElementById("age")
-const travelLenght = document.getElementById("TLenght")
-console.log( name , surname , age , TLenght)
+console.log("surname");
+const age = document.getElementById("age");
+console.log("age");
+const travelLenght = document.getElementById("TLenght");
+console.log("TLenght");
